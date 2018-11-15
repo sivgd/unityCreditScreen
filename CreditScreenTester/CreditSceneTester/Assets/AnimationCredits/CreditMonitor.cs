9ab825604f7741dedf0e5b563de28f8c8c82c8ad
James Bassett <james.bassett@yahoo.com>
2018-11-15 14:58:24 -0800

    Making use of SceneManager.LoadScene

--- CreditScreenTester/CreditSceneTester/Assets/AnimationCredits/CreditMonitor.cs
+++ CreditScreenTester/CreditSceneTester/Assets/AnimationCredits/CreditMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditMonitor : MonoBehaviour {
     
@@ -15,7 +16,9 @@ public class CreditMonitor : MonoBehaviour {
         
         if (Input.GetKeyDown(KeyCode.C))
         {
-            Application.LoadLevel("AniCreditScene");
+            //Application.LoadLevel("AniCreditScene");
+            SceneManager.LoadScene("AniCreditScene"); //Requires "Using" (see above)
+            
         }
     }
 }
