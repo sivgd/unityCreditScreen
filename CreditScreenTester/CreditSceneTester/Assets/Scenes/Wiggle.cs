faa82a00f6ccf43c9db3f0e9e4f58830b4726ff6
James Bassett <james.bassett@yahoo.com>
2018-11-15 14:51:41 -0800

    Added simple keyboard control to 2d square

--- CreditScreenTester/CreditSceneTester/Assets/Scenes/Wiggle.cs
+++ CreditScreenTester/CreditSceneTester/Assets/Scenes/Wiggle.cs
@@ -11,6 +11,10 @@ public class Wiggle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+       
+        transform.Translate(x, y, 0);
+    }
 }
